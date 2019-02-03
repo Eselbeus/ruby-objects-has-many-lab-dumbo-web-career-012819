41692d1e77f287cf19c6900eca237a80e52ceb9a
eselbeus <eselbeus@gmail.com>
2019-02-03 20:28:19 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,9 +15,16 @@ class Author
   end
   
   def add_post(pst)
-    
+    pst.author = self
     @posts << pst
     @@posts << self
+  end
+  
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    new_post.author = self 
+    @posts << new_post
+    @@posts << new_post
   end
   
   def self.all 
