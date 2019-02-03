8de1630db2bc4c78ccc525751455f982379be1ac
eselbeus <eselbeus@gmail.com>
2019-02-03 20:15:25 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Author 
+  
+  attr_accessor :name
+  
+  @@all = []
+  @@posts = []
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+    
+  end
+  
+  def add_post(pst)
+    
+    @posts << pst
+    @@posts << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.post_count
+    @@posts.length
+  end
+  
+end
